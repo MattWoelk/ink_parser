bf65625ae35d88498edf316cbdb128369c61444d
MattWoelk <matt.woelk@gmail.com>
2020-08-02 10:20:00 -0500

    basic header parsing (incomplete)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,39 @@
+use nom::bytes::complete::{take_till, take_till1, take_while};
+use nom::{bytes::complete::tag, IResult};
+
+pub fn parse_knot_header(input: &str) -> IResult<&str, String> {
+    let (input, _) = tag("==")(input)?;
+    let (input, _) = take_while(|c| c == '=')(input)?;
+    let (input, _) = take_till1(|c| c != ' ' && c != '\t')(input)?;
+
+    let (input, title) = take_till1(|c| c == '=' || c == '\n' || c == '\r')(input)?;
+
+    dbg!(input);
+
+    // TODO: parse "space_then_word" (a bunch of these) so we know when to stop before more equals
+
+    let (input, _) = take_till(|c| c == '\r' || c == '\n')(input)?;
+
+    dbg!(input);
+
+    Ok((input, title.to_string()))
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_parse_knot_header() {
+        assert_eq!(
+            parse_knot_header("== station"),
+            Ok(("", "station".to_string()))
+        );
+
+        // TODO:
+        //assert_eq!(
+        //    parse_knot_header("== station =="),
+        //    Ok(("", "station".to_string()))
+        //);
     }
 }
---
+++ stories/basic_story.ink
@@ -0,0 +1,17 @@
+Want to go to paris?
++ yeah!
+-> paris
++ no.
+-> ending
+
+
+=== paris
+
+We are in paris.
+-> ending
+
+
+=== ending
+
+THE END now.
+-> END
