a770cfb66fb24c51d2bc6c28d0a412aad001728e
Matt Woelk <matt@mattwoelk.ca>
2020-12-04 16:14:42 -0600

    Knot struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,18 @@ use nom::combinator::opt;
 use nom::sequence::{preceded, terminated, tuple};
 use nom::IResult;
 
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Knot {
+    title: String,
+    lines: Vec<String>,
+}
+
+impl Default for Knot {
+    fn default() -> Self {
+        todo!()
+    }
+}
+
 /// whitespace that is not a newline character
 fn nbsp_opt(input: &str) -> IResult<&str, &str> {
     take_while(|c| " \t".contains(c))(input)
@@ -31,6 +43,23 @@ pub fn parse_knot_header(input: &str) -> IResult<&str, &str> {
 pub fn parse_text_line(input: &str) -> IResult<&str, &str> {
     let (input, text) = alt((take_until("->"), take_while(|c| !"\r\n".contains(c))))(input)?;
     Ok((input, text))
+}
+
+pub fn parse_knot(input: &str) -> IResult<&str, Knot> {
+    let (input, title) = parse_knot_header(input)?;
+
+    // TODO: parse as many lines as we can find, and deal with empty lines.
+    // TODO: parse diverts (-> London) and throw those in here too, intelligently
+
+    let (input, line) = parse_text_line(input)?;
+
+    Ok((
+        input,
+        Knot {
+            title: title.into(),
+            lines: vec![line.into()],
+        },
+    ))
 }
 
 #[cfg(test)]
@@ -77,6 +106,20 @@ mod tests {
         assert_eq!(
             parse_text_line("a line of text -> end"),
             Ok(("-> end", "a line of text "))
+        );
+    }
+
+    #[test]
+    fn test_knot() {
+        assert_eq!(
+            parse_knot("===train_station===\na line of text\n"),
+            Ok((
+                "\n",
+                Knot {
+                    title: "train_station".into(),
+                    lines: vec!["a line of text".into()],
+                }
+            ))
         );
     }
 }
