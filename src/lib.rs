7fbefba001fe596de8ed624798fc8695c0a99bea
Matt Woelk <matt@mattwoelk.ca>
2021-04-11 21:40:38 -0500

    Can parse a simple story! Tests pass!

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use combine::parser::repeat::{skip_until, take_until};
 use combine::parser::sequence::skip;
 use combine::{
     any, between, chainl1, look_ahead, none_of, not_followed_by, parser, satisfy, skip_count,
+    EasyParser,
 };
 use combine::{
     attempt, eof, many, many1, optional, sep_by, sep_by1, skip_many1, token, ParseError, Parser,
@@ -17,8 +18,10 @@ use combine::{
 use maplit::btreemap;
 use pretty_assertions::{assert_eq, assert_ne};
 use std::collections::BTreeMap;
+use std::unreachable;
 
 // TODO: diverts should only parse one word, don't use "line()" for everything.
+// TODO: pass state along, so when parsing fails I can debug it.
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Knot {
@@ -50,12 +53,12 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    spaces().with(
-        not_followed_by(char('+').or(char('-'))).with(
+    not_followed_by(char('+').or(char('-')))
+        .expected("wut?")
+        .with(
             many1::<String, _, _>(satisfy(|c| c != '\n' && c != '\r'))
                 .skip(optional(char('\n').or(char('\r').skip(char('\n'))))),
-        ),
-    )
+        )
 }
 
 fn lines<'a, Input>() -> impl Parser<Input, Output = Vec<String>>
@@ -63,13 +66,14 @@ where
     Input: RangeStream<Token = char, Range = &'a str>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    many1(line().map(|s| s.into()))
+    spaces().with(many1(line().skip(spaces()).map(|s| s.into())))
 }
 
 type Divert = String;
 type Choice = (String, Option<Vec<String>>, Option<Divert>);
 
 // TODO: variables, conditionals, etc.
+// TODO: should the choice_lines (etc.) function return a piece of these, but have them be able to be cast to this main enum or something? What's the right way to do this?
 #[derive(Debug, PartialEq, Eq, Clone)]
 enum LineTypes {
     TEXT(String),
@@ -88,28 +92,26 @@ where
         .map(|a| LineTypes::DIVERT(a))
 }
 
-fn choice_line<'a, Input>() -> impl Parser<Input, Output = LineTypes>
+fn choice_lines<'a, Input>() -> impl Parser<Input, Output = LineTypes>
 where
     Input: RangeStream<Token = char, Range = &'a str>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    spaces().with(
-        char('+')
-            .and(many::<String, _, _>(char(' ')))
-            .with(line())
-            .and(optional(lines()))
-            .and(optional(divert_line()))
-            .map(|((title, lines), divert)| {
-                LineTypes::CHOICE((
-                    title,
-                    lines,
-                    divert.map(|divert| match divert {
-                        LineTypes::DIVERT(text) => text,
-                        _ => "".to_string(),
-                    }),
-                ))
-            }),
-    )
+    char('+')
+        .and(many::<String, _, _>(char(' ')))
+        .with(line())
+        .and(optional(lines()))
+        .and(optional(divert_line()))
+        .map(|((title, lines), divert)| {
+            LineTypes::CHOICE((
+                title,
+                lines,
+                divert.map(|divert| match divert {
+                    LineTypes::DIVERT(text) => text,
+                    _ => "".to_string(),
+                }),
+            ))
+        })
 }
 
 fn knot_body_lines<'a, Input>() -> impl Parser<Input, Output = Vec<LineTypes>>
@@ -119,7 +121,7 @@ where
 {
     many1::<Vec<LineTypes>, _, _>(choice((
         divert_line(),
-        choice_line(),
+        choice_lines(),
         line().map(|x| LineTypes::TEXT(x)),
     )))
 }
@@ -147,7 +149,7 @@ where
         .map(|(knot_title, knot)| Knot {
             title: knot_title,
             lines: knot.lines,
-            choices: vec![], // TODO
+            choices: knot.choices,
             divert: knot.divert,
         })
 }
@@ -159,16 +161,25 @@ where
 {
     spaces()
         .with(optional(lines()))
-        .and(optional(many::<Vec<LineTypes>, _, _>(choice_line())))
+        .and(optional(many::<Vec<LineTypes>, _, _>(choice_lines())))
         .and(optional(divert_line()))
-        .map(|((a, b), c)| Knot {
+        .map(|((lines, choices), divert)| Knot {
             title: "INTRO".to_string(),
-            lines: match a {
+            lines: match lines {
                 Some(lines) => lines,
                 None => vec![],
             },
-            choices: vec![], // TODO
-            divert: c.map(|x| match x {
+            choices: match choices {
+                Some(choices) => choices
+                    .into_iter()
+                    .map(|x| match x {
+                        LineTypes::CHOICE(c) => c,
+                        _ => unreachable!(),
+                    })
+                    .collect(),
+                None => vec![],
+            },
+            divert: divert.map(|x| match x {
                 LineTypes::DIVERT(divert) => divert,
                 _ => "".to_string(),
             }),
@@ -196,7 +207,7 @@ where
 }
 
 pub fn parse_story(text: &str) -> Story {
-    story().parse(text).unwrap().0
+    story().easy_parse(text).unwrap().0
 }
 
 #[derive(PartialEq, Debug)]
@@ -253,36 +264,52 @@ fn test_line() {
         Err(StringStreamError::UnexpectedParse)
     );
     assert_eq!(
-        line().parse("no line endings"),
+        line().easy_parse("no line endings"),
         Ok(("no line endings".to_string(), ""))
     );
     assert_eq!(
-        line().parse("one line ending\n"),
+        line().easy_parse("one line ending\n"),
         Ok(("one line ending".to_string(), ""))
     );
     assert_eq!(
-        line().parse("both line endings\r\n"),
+        line().easy_parse("both line endings\r\n"),
         Ok(("both line endings".to_string(), ""))
     );
     assert_eq!(
-        line().parse("          line starting with spaces\r\n"),
-        Ok(("line starting with spaces".to_string(), ""))
+        line().easy_parse("          line starting with spaces\r\n"),
+        Ok(("          line starting with spaces".to_string(), ""))
     );
     assert_eq!(
-        line().parse("       \n \r\n   line starting with newlines and spaces\r\n"),
-        Ok(("line starting with newlines and spaces".to_string(), ""))
+        line().easy_parse("       \n \r\n   line starting with newlines and spaces\r\n"),
+        Ok((
+            "       ".to_string(),
+            " \r\n   line starting with newlines and spaces\r\n"
+        ))
     );
+}
+
+#[test]
+fn test_divert() {
+    assert_eq!(
+        divert_line().easy_parse("-> yeah"),
+        Ok((LineTypes::DIVERT(("yeah".to_string())), ""))
+    );
+
+    //assert_eq!(
+    //    divert_line().easy_parse("    -> yeah"),
+    //    Ok((LineTypes::DIVERT(("yeah".to_string())), ""))
+    //);
 }
 
 #[test]
 fn test_choice() {
     assert_eq!(
-        choice_line().parse("+ yeah"),
+        choice_lines().easy_parse("+ yeah"),
         Ok((LineTypes::CHOICE(("yeah".to_string(), None, None)), ""))
     );
 
     assert_eq!(
-        choice_line().parse("+ yeah\n  one\ntwo\n     three"),
+        choice_lines().easy_parse("+ yeah\n  one\ntwo\n     three"),
         Ok((
             LineTypes::CHOICE((
                 "yeah".to_string(),
@@ -298,7 +325,29 @@ fn test_choice() {
     );
 
     assert_eq!(
-        choice_line().parse("+ yeah\n  one\ntwo\n-> paris"),
+        choice_lines().easy_parse("+ yeah\n  one\ntwo\n-> paris"),
+        Ok((
+            LineTypes::CHOICE((
+                "yeah".to_string(),
+                Some(vec!["one".to_string(), "two".to_string(),]),
+                Some("paris".to_string())
+            )),
+            ""
+        ))
+    );
+
+    assert_eq!(
+        lines().easy_parse("  one\ntwo\n   + paris"),
+        Ok((vec!["one".to_string(), "two".to_string(),], "+ paris"))
+    );
+
+    assert_eq!(
+        lines().easy_parse("  one\ntwo\n   -> paris"),
+        Ok((vec!["one".to_string(), "two".to_string(),], "-> paris"))
+    );
+
+    assert_eq!(
+        choice_lines().easy_parse("+ yeah\n  one\ntwo\n   -> paris"),
         Ok((
             LineTypes::CHOICE((
                 "yeah".to_string(),
@@ -313,7 +362,7 @@ fn test_choice() {
 #[test]
 fn test_story() {
     assert_eq!(
-        story().parse(include_str!("../stories/two_knots.ink")),
+        story().easy_parse(include_str!("../stories/two_knots.ink")),
         Ok((
             Story {
                 knots: btreemap! {
@@ -348,17 +397,20 @@ fn test_story() {
     );
 
     assert_eq!(
-        story().parse(include_str!("../stories/two_knots_with_choices.ink")),
+        story().easy_parse(include_str!("../stories/two_knots_with_choices.ink")),
         Ok((
             Story {
                 knots: btreemap! {
                     "INTRO".to_string() => Knot {
                         title: "INTRO".to_string(),
                         lines: vec![
-                            "to paris".to_string()
+                            "to paris?".to_string()
                         ],
-                        choices: vec![],
-                        divert: Some("paris".to_string()),
+                        choices: vec![
+                            ("yeah".to_string(), Some(vec!["yes, please".to_string()]), Some("paris".to_string())),
+                            ("no".to_string(), Some(vec!["no, thank you".to_string()]), Some("ending".to_string())),
+                        ],
+                        divert: None,
                     },
                     "paris".to_string() => Knot {
                         title: "paris".to_string(),
